3c36b99660569c16700861da6f2b2d2bf7185d3b
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2023-01-30 00:50:29 +0300

    Add files via upload

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,8 +6,10 @@ int main()
     Point const b(2,5);
     Point const c(5,4);
 
-    Point const p(100,3);
+    Point const p(3,3);
 
+    //the purpose of this algorithm is if a point is inside a triangle the areas are equal
+    //if the point is outside the triangle, its area is greater than the area of ​​the triangle
     bool is = bsp(a, b, c, p);
 
     if (is == true)
