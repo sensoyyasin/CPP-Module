4426192e3648bed7491e20fd87a8bc2bfd3e6e58
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2023-02-03 09:36:09 +0300

    Add files via upload

--- CPP03/ex03/ClapTrap.hpp
+++ CPP03/ex03/ClapTrap.hpp
@@ -16,22 +16,22 @@ public:
     ~ClapTrap();
 
     //GETTER AND SETTER FOR print the private variables
-    virtual std::string getName();
-    virtual void setName(const std::string &Name);
+    std::string getName();
+    void setName(const std::string &Name);
 
-    virtual void setHitPoints(int hitPoints);
-    virtual int getHitPoints();
+    void setHitPoints(int hitPoints);
+    int getHitPoints();
 
-    virtual void setAttackDamage(int attack);
-    virtual int getAttackDamage();
+    void setAttackDamage(int attack);
+    int getAttackDamage();
 
-    virtual void setEnergyPoint(int energyPoint);
-    virtual int getEnergyPoint();
+    void setEnergyPoint(int energyPoint);
+    int getEnergyPoint();
 
     //functions
-    virtual void attack(const std::string& target);
-    virtual void takeDamage(unsigned int amount);
-    virtual void beRepaired(unsigned int amount);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
     
 };
 
